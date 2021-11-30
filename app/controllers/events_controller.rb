86702528112310a7a1677f076180f1719e2b67aa
Pedro Silva <pacs@outlook.pt>
2021-11-30 13:19:56 +0000

    first iteration of events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,15 @@
 class EventsController < ApplicationController
+
+  def index
+    @featured_event = Event.all.sample(5)
+    @events = Event.all
+    @categories = Category.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+    @event_category = @event.category
+    @category_events = @event_category.events
+    raise
+  end
 end
