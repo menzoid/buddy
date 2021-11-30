128f1b05f49bbc55c5c2f7e021b132271d304465
Pedro Silva <pacs@outlook.pt>
2021-11-30 16:16:14 +0000

    boostrap working

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :events
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :categories, only: :show
+  resources :categories, only: %i[show index]
 end
