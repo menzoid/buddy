00aa258709ddc25ee2a475eecb9bdf54ff0dc3da
Pedro Silva <pacs@outlook.pt>
2021-12-02 16:56:36 +0000

    added maps untested

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,12 +10,18 @@ class EventsController < ApplicationController
     end
     respond_to do |format|
       format.html # Follow regular flow of Rails
-      format.text { render partial: 'list.html', locals: { events: @pg_search_events} }
+      format.text { render partial: 'list.html', locals: { events: @pg_search_events } }
     end
   end
 
 
   def show
     @event = Event.find(params[:id])
+    @markers = [
+      {
+        lat: @event.latitude,
+        lng: @event.longitude
+      }
+    ]
   end
 end
