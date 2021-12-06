72b9262f7795225e4ef5239943b1e8fdb599850d
Ryan Brouwer <mennofromheavan@gmail.com>
2021-12-06 12:40:52 +0000

    have added sessions and registrations controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,7 @@ class ApplicationController < ActionController::Base
   #   devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   # end
 
+
   private
 
   def skip_pundit?
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
   has_many :bookings
   has_many :chatroom_users
   has_many :chatrooms, through: :chatroom_users
@@ -11,4 +10,5 @@ class User < ApplicationRecord
   has_one_attached :photo
   has_many :requests_as_requestee, through: :bookings, foreign_key: :requestee_id
   has_many :requests_as_requester, through: :bookings, foreign_key: :requester_id
+
 end
