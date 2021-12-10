de25b2db0e03f268cadd97a5266eeec2058e631e
Ryan Brouwer <mennofromheavan@gmail.com>
2021-12-10 10:40:08 +0000

    navbar colours working

--- app/javascript/plugins/navbar_highlight.js
+++ app/javascript/plugins/navbar_highlight.js
@@ -1,19 +1,22 @@
 const navLight = () => {
   const nav = document.querySelector('.navbar-buddy');
-  const btns = nav.querySelectorAll('.highlight');
+  const btns = nav.querySelectorAll('.nav-icon');
   for (var i = 0; i < btns.length; i++) {
     btns[i].addEventListener("click", function () {
       const current = document.getElementsByClassName("shine");
       console.log(current);
       // If there's no active class
       if (current.length > 0) {
-        current[0].className = current[0].className.replace(" shine", "");
+        current[0].className = current[0].className.toggle("shine");
       }
 
       // Add the active class to the current/clicked button
-      this.className += " shine";
+      this.className += "shine";
     });
   }
 };
 
 export { navLight };
+
+
+// if btn find class shine exists then toggle, this.classname =
